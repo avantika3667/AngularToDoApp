da8fd0e1ea5c3e7d2c6ce3c103ca0a5ef67298bd
avantika <avantika.karwasara@snapdeal.com>
2017-02-23 16:05:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var app = angular.module('myApp', []);
+app.controller('todoCtrl', function ($scope) {
+    $scope.todoList = [];
+
+    $scope.todoAdd = function () {
+        $scope.todoList.push({ taskName: $scope.todoInput, done: false });
+        $scope.todoInput = "";
+        $scope.hideAddForm();
+    };
+
+    $scope.remove = function () {
+        angular.forEach($scope.todoList, function (val, key) {
+            if (val.done) {
+                $scope.todoList.splice(key, 1);
+            }
+        });
+    };
+    $scope.delete = function (index) {
+        $scope.todoList.splice(index, 1);
+    }
+
+    $scope.addToDo = function () {
+        document.getElementById('fade').style.display = 'block'
+        document.getElementById('light').style.display = 'block';
+
+    }
+    $scope.hideAddForm = function () {
+        document.getElementById('light').style.display = 'none';
+        document.getElementById('fade').style.display = 'none'
+    }
+});
+
